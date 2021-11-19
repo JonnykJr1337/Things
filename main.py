f12fe3c1e2fb416419018d4d6e8f6eb77b9822a0
JonnykJr1337 <94223354+JonnykJr1337@users.noreply.github.com>
2021-11-19 22:41:38 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import sys, sqlite3
 
-
 from PyQt5 import uic
 from PyQt5.QtCore import Qt, QThread
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMainWindow, QLayout, QPushButton, QDialog, QLineEdit
-from PyQt5.QtGui import QPixmap, QTransform, QFont
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QListWidgetItem
 
 
 class Example(QMainWindow):
@@ -13,89 +11,60 @@ class Example(QMainWindow):
         self.db = sqlite3.connect('database.db')
         self.cur = self.db.cursor()
         uic.loadUi('untitled.ui', self)
-        self.loginbutton.clicked.connect(self.login)
-        self.regbutton.clicked.connect(self.registration)
-        self.menubutton.clicked.connect(self.openmenu)
-        self.booksbutton.clicked.connect(self.books)
+        self.pages.setCurrentIndex(0)
+        self.okbutton.clicked.connect(self.checkuser)
+        self.okok.clicked.connect(self.registeruser)
+        self.nono.clicked.connect(lambda: self.pages.setCurrentIndex(0))
+        self.back.clicked.connect(lambda: self.pages.setCurrentIndex(0))
+        self.cancelbutton.clicked.connect(lambda: self.pages.setCurrentIndex(0))
+        self.loginbutton.clicked.connect(lambda: self.pages.setCurrentIndex(2))
+        self.regbutton.clicked.connect(lambda: self.pages.setCurrentIndex(1))
+        self.booksbutton.clicked.connect(lambda: self.pages.setCurrentIndex(3))
+        self.booksbutton.clicked.connect(self.searchingbooks)
         self.exitbutton.clicked.connect(self.exit)
-        self.exitbutton.hide()
-        self.regbutton.hide()
-        self.loginbutton.hide()
-        self.booksbutton.hide()
-        self.currentuser = (self.cur.execute("""select username from last_user where id = 1""").fetchall())[0][0]
-        self.message = QLabel('', self)
+        self.back_2.clicked.connect(lambda: self.pages.setCurrentIndex(3))
+        self.addbook.clicked.connect(self.addbookk)
+        self.userbooks.itemDoubleClicked.connect(self.monitorim)
+        self.currentuser = (self.cur.execute("""select username from last_user""").fetchall())[0][0]
 
+    def monitorim(self):
+        self.pages.setCurrentIndex(5)
+        self.showbook.setHtml((
+                                  self.cur.execute("""select html from books where title = ?""",
+                                                   (self.userbooks.selectedItems()[0].text(),)).fetchall())[0][0])
 
-    def login(self):
-        self.dialog = QDialog()
-        self.dialog.setWindowTitle("Вход")
-        self.dialog.resize(220, 220)
-        self.logtext = QLabel('Логин:', self.dialog)
-        self.pastext = QLabel('Пароль:', self.dialog)
-        self.answer = QLabel('', self.dialog)
-        self.login = QLineEdit(self.dialog)
-        self.password = QLineEdit(self.dialog)
-        okbtn = QPushButton('OK', self.dialog)
-        cancbtn = QPushButton('Cancel', self.dialog)
-        self.logtext.move(10, 55)
-        self.pastext.move(10, 85)
-        self.answer.resize(200, 20)
-        self.answer.move(10, 160)
-        self.login.resize(150, 20)
-        self.login.move(60, 50)
-        self.password.resize(150, 20)
-        self.password.move(60, 80)
-        okbtn.resize(90, 20)
-        okbtn.move(5, 190)
-        cancbtn.resize(90, 20)
-        cancbtn.move(125, 190)
-        okbtn.clicked.connect(self.checkuser)
-        cancbtn.clicked.connect(self.closething)
-        self.dialog.setWindowModality(Qt.ApplicationModal)
-        self.dialog.exec_()
+    def addbookk(self):
+        check = False
+        self.pages.setCurrentIndex(4)
+        self.booklist.itemDoubleClicked.connect(self.addbooktouser)
+        self.cancelforbooks.clicked.connect(lambda: self.pages.setCurrentIndex(3))
+        self.okforbooks.clicked.connect(lambda: self.pages.setCurrentIndex(3))
+        self.okforbooks.clicked.connect(self.searchingbooks)
+        books = self.cur.execute("""select title from books""")
+        for elem in books:
+            for elem1 in self.booklist.selectedItems():
+                if elem[0] == elem1.text():
+                    check = True
+            if not check:
+                self.booklist.addItem(elem[0])
+            else:
+                check = False
 
-    def registration(self):
-        self.dialog = QDialog()
-        self.dialog.setWindowTitle("Регистрация")
-        self.dialog.resize(220, 220)
-        self.logtext = QLabel('Логин:', self.dialog)
-        self.pastext = QLabel('Пароль:', self.dialog)
-        self.emailtext = QLabel('Почта:', self.dialog)
-        self.answer = QLabel('', self.dialog)
-        self.email = QLineEdit(self.dialog)
-        self.login = QLineEdit(self.dialog)
-        self.password = QLineEdit(self.dialog)
-        okbtn = QPushButton('OK', self.dialog)
-        cancbtn = QPushButton('Cancel', self.dialog)
-        self.emailtext.move(10, 25)
-        self.logtext.move(10, 55)
-        self.pastext.move(10, 85)
-        self.answer.resize(200, 20)
-        self.answer.move(10, 160)
-        self.email.resize(150, 20)
-        self.email.move(60, 20)
-        self.login.resize(150, 20)
-        self.login.move(60, 50)
-        self.password.resize(150, 20)
-        self.password.move(60, 80)
-        okbtn.resize(90, 20)
-        okbtn.move(5, 190)
-        cancbtn.resize(90, 20)
-        cancbtn.move(125, 190)
-        okbtn.clicked.connect(self.registeruser)
-        cancbtn.clicked.connect(self.closething)
-        self.dialog.setWindowModality(Qt.ApplicationModal)
-        self.dialog.exec_()
+    def addbooktouser(self):
+        self.cur.execute("""insert into has_book (id_user, id_book) values ((select id from users where login = ?), 
+        (select id from books where title = ?))""", (self.currentuser, self.booklist.selectedItems()[0].text()))
+        self.db.commit()
 
     def exit(self):
         self.currentuser = '0'
-        self.cur.execute("""update last_user set username='0' where id=1""").fetchall()
+        self.cur.execute("""update last_user set username='0'""").fetchall()
         self.db.commit()
+        self.close()
 
     def checkuser(self):
         log = self.login.text()
         pas = self.password.text()
-        password = self.cur.execute("""select password from users where login = ?""", (log, )).fetchall()
+        password = self.cur.execute("""select password from users where login = ?""", (log,)).fetchall()
         if len(password) == 0:
             self.answer.setText('Несуществующий логин')
         elif self.currentuser == log:
@@ -103,76 +72,59 @@ class Example(QMainWindow):
         else:
             if pas == password[0][0]:
                 self.currentuser = log
-                self.dialog.setVisible(False)
-                self.cur.execute("""update last_user set username=? where id=1""", (log, )).fetchall()
-                self.message.hide()
-                self.hellolabel.hide()
+                self.cur.execute("""update last_user set username=?""", (log,)).fetchall()
+                self.pages.setCurrentIndex(0)
             else:
                 self.answer.setText('Неправильный пароль')
         self.db.commit()
 
     def registeruser(self):
-        log = self.login.text()
-        pas = self.password.text()
-        em = self.email.text()
+        log = self.reglogin.text()
+        pas = self.regpassword.text()
+        em = self.regemail.text()
         i = (self.cur.execute("""select id from users where id=(select max(id) from users)""").fetchall())[0][0]
-        login = self.cur.execute("""select login from users where login = ?""", (log, )).fetchall()
-        email = self.cur.execute("""select login from users where login = ?""", (em, )).fetchall()
-        self.cur.execute("""update last_user set username=? where id=1""", (log, )).fetchall()
+        login = self.cur.execute("""select login from users where login = ?""", (log,)).fetchall()
+        email = self.cur.execute("""select login from users where login = ?""", (em,)).fetchall()
+        self.cur.execute("""update last_user set username=?""", (log,)).fetchall()
         if len(email) == 0:
             if len(login) == 0:
                 if len(log) >= 8:
-                    self.message.hide()
-                    self.hellolabel.hide()
-                    self.cur.execute("""insert into users (id, email, login, password) values (?, ?, ?, ?)""", ((i + 1), em, log, pas)).fetchall()
-                    self.cur.execute("""update last_user set username=? where id=1""", (log, )).fetchall()
+                    self.cur.execute("""insert into users (id, email, login, password) values (?, ?, ?, ?)""",
+                                     ((i + 1), em, log, pas)).fetchall()
+                    self.cur.execute("""update last_user set username=?""", (log,)).fetchall()
                     self.currentuser = log
-                    self.dialog.setVisible(False)
+                    self.pages.setCurrentIndex(0)
                 else:
                     self.answer.setText('Логин должен быть больше 7 символов')
             else:
                 self.answer.setText('Придумайте другой логин')
         else:
-            self.answer.setText('Эта почта зарегестрирована')
+            self.answer.setText('Эта почта зарегистрирована')
         self.db.commit()
 
-    def closething(self):
-        self.dialog.setVisible(False)
-
-    def openmenu(self):
-        self.loginbutton.show()
-        self.regbutton.show()
-        self.booksbutton.show()
-        self.exitbutton.show()
-        self.menubutton.hide()
-
-    def books(self):
-        self.message.hide()
-        self.regbutton.hide()
-        self.loginbutton.hide()
-        self.booksbutton.hide()
-        self.exitbutton.hide()
-        self.menubutton.show()
+    def searchingbooks(self):
         if self.currentuser == '0':
             self.hellolabel.setText('')
-            self.message = QLabel('Войдите в аккаунт чтобы получить книжки', self)
-            self.message.setFont(QFont('MS Shell Dlg 2', 12))
-            self.message.resize(320, 31)
-            self.message.move(140, 61)
-            self.message.show()
+            self.messagelabel.setText('Войдите в аккаунт чтобы получить книжки')
         else:
             self.hellolabel.setText(f'Привет, {self.currentuser}, вот твои книжки')
+            self.messagelabel.setText('')
             books = self.cur.execute("""select title from books where id=
                                         (select id_book from has_book where id_user=
-                                        (select id from users where login=?))""", (self.currentuser, )).fetchall()
+                                        (select id from users where login=?))""", (self.currentuser,)).fetchall()
             if len(books) == 0:
-                self.message = QLabel('К сожалению, у вас нет ни одной книжки :(', self)
-                self.message.setFont(QFont('MS Shell Dlg 2', 10))
-                self.message.resize(260, 31)
-                self.message.move(170, 61)
-                self.message.show()
+                self.messagelabel.setText('К сожалению, у вас нет ни одной книжки :(')
             else:
-                pass
+                check = False
+                for elem in books:
+                    for elem1 in self.userbooks.selectedItems():
+                        if elem[0] == elem1.text():
+                            check = True
+                    if not check:
+                        self.userbooks.addItem(elem[0])
+                    else:
+                        check = False
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
